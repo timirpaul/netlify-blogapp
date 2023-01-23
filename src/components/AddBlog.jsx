9f7c8d9eef0d997cc81ccf964e1c4543ea109f72
timirpaul <89598473+timirpaul@users.noreply.github.com>
2023-01-24 01:42:50 +0530

    add render.com path

--- src/components/AddBlog.jsx
+++ src/components/AddBlog.jsx
@@ -21,8 +21,8 @@ const TextArea = styled(TextareaAutosize)`
 `
 
 const AddBlog = () => {
-
-    const apiURL = "https://blog-app-api-server.herokuapp.com/api"
+    const apiURL = "https://blogs-app-pwvr.onrender.com/api";
+//     const apiURL = "https://blog-app-api-server.herokuapp.com/api"
    
     // const apiURL = "http://localhost:5000/api"
     
