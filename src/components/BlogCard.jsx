5e3f93e81191d30ee48e37e8994e0d6e5dd6959c
timirpaul <89598473+timirpaul@users.noreply.github.com>
2023-01-24 01:44:50 +0530

    add render.com path

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -8,9 +8,10 @@ import { useSelector } from "react-redux";
 
 
 const BlogCard = ({ id, title, description, username, categories, createdAt, photo, isUser }) => {
-
-        const apiURL = "https://blog-app-api-server.herokuapp.com/api"
-        const imgAPIURL = "https://blog-app-api-server.herokuapp.com/images/"
+        const apiURL = "https://blogs-app-pwvr.onrender.com/api";
+  const imgAPIURL = "https://blogs-app-pwvr.onrender.com/images/";
+//         const apiURL = "https://blog-app-api-server.herokuapp.com/api"
+//         const imgAPIURL = "https://blog-app-api-server.herokuapp.com/images/"
 
         // const apiURL = "http://localhost:5000/api"
         // const imgAPIURL = "http://localhost:5000/images/"
